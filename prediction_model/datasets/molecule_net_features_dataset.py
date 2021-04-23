Fixed calculation of number of bonds per atom and removed prints

prediction_model/datasets/molecule_net_features_dataset.py
@@ -107,7 +107,6 @@ class MoleculeNetFeaturesDataset(MoleculeNet):
                 continue
 
             xs = []
-            num_bonds_per_atom = []
             for atom in mol.GetAtoms():
                 x = []
                 x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
@@ -122,12 +121,11 @@ class MoleculeNetFeaturesDataset(MoleculeNet):
                 x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
                 x.append(x_map['is_in_ring'].index(atom.IsInRing()))
                 xs.append(x)
-                num_bonds_per_atom.append(int(len(atom.GetBonds()) / 2))
 
             x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
-            num_bonds_per_atom = torch.tensor(num_bonds_per_atom, dtype=torch.int)
             num_atoms_per_mol = torch.tensor([len(mol.GetAtoms())], dtype=torch.int)
 
+            num_bonds_per_atom = [0] * len(mol.GetAtoms())
             edge_indices, edge_attrs = [], []
             for bond in mol.GetBonds():
                 i = bond.GetBeginAtomIdx()
@@ -141,9 +139,13 @@ class MoleculeNetFeaturesDataset(MoleculeNet):
                 edge_indices += [[i, j], [j, i]]
                 edge_attrs += [e, e]
 
+                num_bonds_per_atom[i] += 1
+                num_bonds_per_atom[j] += 1
+
             edge_index = torch.tensor(edge_indices)
             edge_index = edge_index.t().to(torch.long).view(2, -1)
             edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+            num_bonds_per_atom = torch.tensor(num_bonds_per_atom, dtype=torch.int)
 
             # Sort indices.
             if edge_index.numel() > 0:

prediction_model/generate_hiv_model.py
@@ -41,7 +41,6 @@ def get_data():
 
     # Load the HIV dataset we will be using for this training.
     dataset = MoleculeNetFeaturesDataset(data_path, name="HIV")
-    dataset = dataset[:5120] # TODO
 
     # Compute loss positive weight.
     num_pos = sum([data.y for data in dataset]).detach().item()
@@ -169,7 +168,7 @@ def get_hiv_classifier(num_train_epochs, ensemble_size, torch_device, num_opt_it
             break
 
     # Initialize models. Everything should exist already so don't need data loaders.
-    atom_dim, bond_dim, features_dim = get_dimensions(None)
+    _, _, atom_dim, bond_dim, features_dim = get_data()
     models = generate_initial_hiv_models(ensemble_size, atom_dim, bond_dim, features_dim, 
                                          torch_device, None, None, num_opt_iters, num_train_epochs)
     
@@ -178,4 +177,4 @@ def get_hiv_classifier(num_train_epochs, ensemble_size, torch_device, num_opt_it
         model.load_state_dict(torch.load(get_model_state_dict_path(idx)))
 
     # Create HIV classifier model object and return.
-    return HIVClassifier(models)
+    return HIVClassifier(models).to(torch_device)

prediction_model/models/directed_mpnn.py
@@ -29,22 +29,22 @@ class DMPNNEncoder(nn.Module):
         self.cached_zero_vector = nn.Parameter(torch.zeros(args.hidden_size), requires_grad=False)
 
         # Dropout layer.
-        self.dropout_layer = nn.Dropout(args.dropout_prob).to(torch_device)
+        self.dropout_layer = nn.Dropout(args.dropout_prob)
 
         # Activation function for the linear networks in the model.
-        self.relu = nn.LeakyReLU(inplace=True).to(torch_device)
+        self.relu = nn.LeakyReLU(inplace=True)
 
         # Batch normalization for training.
         self.bn = nn.BatchNorm1d(args.hidden_size)
 
         # Input linear model W_i used to calculate h0.
-        self.W_i = nn.Linear(atom_dim + bond_dim, args.hidden_size).to(torch_device)
+        self.W_i = nn.Linear(atom_dim + bond_dim, args.hidden_size)
 
         # Inner hidden linear model W_m used to calculate h{t+1}.
-        self.W_m = nn.Linear(args.hidden_size, args.hidden_size).to(torch_device)
+        self.W_m = nn.Linear(args.hidden_size, args.hidden_size)
 
         # Output linear model W_a used to calculate h{v}, the molecular representation of the result.
-        self.W_a = nn.Linear(atom_dim + args.hidden_size, args.hidden_size).to(torch_device)
+        self.W_a = nn.Linear(atom_dim + args.hidden_size, args.hidden_size)
 
     """
     Computes the next bond message for the given last hidden state.

prediction_model/models/prediction_model.py
@@ -40,24 +40,25 @@ class PredictionModel(nn.Module):
         # The layers of feed-forward neural networks which will compute the property 
         # prediction from the embedding.
         if (args.num_ffn_layers == 1):
-            ffn = [dropout, nn.Linear(args.hidden_size + features_dim, 1).to(torch_device)]
+            ffn = [dropout, nn.Linear(args.hidden_size + features_dim, 1)]
         else:
             # First layer.
-            ffn = [dropout, nn.Linear(args.hidden_size + features_dim, args.ffn_hidden_size).to(torch_device)]
+            ffn = [dropout, nn.Linear(args.hidden_size + features_dim, args.ffn_hidden_size)]
 
             # Middle layers.
             for _ in range(args.num_ffn_layers - 2):
                 ffn.extend([relu, dropout, 
-                    nn.Linear(args.ffn_hidden_size, args.ffn_hidden_size).to(torch_device)])
+                    nn.Linear(args.ffn_hidden_size, args.ffn_hidden_size)])
             
             # Final layer.
-            ffn.extend([relu, dropout, nn.Linear(args.ffn_hidden_size, 1).to(torch_device)])
+            ffn.extend([relu, dropout, nn.Linear(args.ffn_hidden_size, 1)])
         
         # Create final FFN model.
         self.ffn = nn.Sequential(*ffn).to(torch_device)
 
         # Initialize the weights for the model.
         for param in self.parameters():
+            print(param.names)
             if param.dim() == 1:
                 nn.init.constant_(param, 0)
             else:
@@ -78,6 +79,19 @@ class PredictionModel(nn.Module):
     """
     def forward(self, atom_features, bond_features, bond_index, molecule_features,
                 atom_incoming_bond_map, bond_reverse_map, num_bonds_per_atom, num_atoms_per_mol):
+
+        print("FFN parameters:")
+        for name, param in self.ffn.named_parameters():
+            print(name)
+            print(param.requires_grad)
+            print(param.grad)
+
+        print("Encoder parameters")
+        for name, param in self.encoder.named_parameters():
+            print(name)
+            print(param.requires_grad)
+            print(param.grad)
+
         # Compute prediction.
         output = self.encoder(atom_features, bond_features, bond_index, molecule_features, 
                               atom_incoming_bond_map, bond_reverse_map, num_bonds_per_atom,
@@ -100,11 +114,6 @@ Parameters:
     - torch_device : The device to send the tensors to.
 """
 def get_model_args_from_batch(extended_batch, torch_device):
-    print(extended_batch.atom_incoming_bond_map)
-    print(extended_batch.bond_reverse_map)
-    print(extended_batch.batch)
-    exit()
-
     # Send items from torch geometric batch to torch device.
     atom_features = extended_batch.batch.x.to(torch_device)
     bond_features = extended_batch.batch.edge_attr.to(torch_device)